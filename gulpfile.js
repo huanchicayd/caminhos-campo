7c5da0bfd32b5cdf8c040e510727f36ddd97b7fc
Daniel Huanchicay <huanchicayd@gmail.com>
2018-01-23 22:50:22 -0200

    Ajustes de diretórios

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    imageminJpegRecompress = require('imagemin-jpeg-recompress'),
+    clean = require('gulp-clean'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin'),
+    htmlmin = require('gulp-htmlmin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    babel = require('gulp-babel'),
+    useref = require('gulp-useref'),
+    inlineSource = require('gulp-inline-source');
+
+gulp.task('default', ['copy'], function(){
+    gulp.start('build-img', 'build-js', 'build-css', 'build-html');
+});
+
+gulp.task('copy', ['clean'], function(){
+    return gulp.src([
+        'dev/**/*',
+        '!dev/styles/**'
+    ])
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function(){
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build-img', function(){
+    gulp.src('dev/images/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle(),
+            imageminJpegRecompress({
+                loops: 6,
+                min: 40,
+                max: 85,
+                quality: 'low'
+            }),
+            imagemin.optipng(),
+            imagemin.svgo()
+        ]))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('build-css', function(){
+    gulp.src('dev/styles/*.css')
+        .pipe(cssmin())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('build-js', function(){
+    gulp.src('dev/scripts/**/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('build-html', function(){
+    return gulp.src('dev/*.html')
+        .pipe(useref())
+        .pipe(inlineSource())
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'));
+});
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Caminhos do Campo
+## Plataforma web de compra de cestas
+###### Desenvolvido com Natio Criativo
+
+## Technologies Used:
+* HTML5
+* CSS3
+* Sass
+* Bootstrap Grid
+* Normalize
+* Slick Slider
+* Fancy Box
+* Javascript ES6
+* Performance e Otimização
+* Gulp
+
+## Prototype:
+
+## Oficial site:
